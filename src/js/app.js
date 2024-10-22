0f937f65ca002398d2e5081121e4edb84ac27ca9
Aleksandr Maltsev <mad250793@yandex.ru>
2024-10-23 01:05:03 +0300

    fff

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,18 @@
 import '@/css/tailwind.css';
 
-const a = async () =>{
-     console.log('async work');
+import '@/js/vars'; // global vars 
+import '@/js/service-worker'; // enable service-worker
+
+console.log('Check import functions:', $("body"));
+
+const testAsync =  async () => {
+   await console.log('Check async funtions: Ok')
 }
 
-a()
-console.log(a);
+testAsync()
 
+try {
+   console.log('Check babel job:', testAsync);
+} catch (error) {
+      console.log('Error', error);
+}
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,20 @@ import path from 'path';
 import babel from '@rollup/plugin-babel';
 
 export default defineConfig({
+
+    build: {
+      outDir: './build',
+      emptyOutDir: true, // also necessary
+      rollupOptions: {
+        output: {
+          entryFileNames: `app.min.js`,
+          assetFileNames: `app.min.[ext]`,
+        },
+      },
+    },  
+    server: {
+        port: 3000, // ваш настраиваемый порт
+    },
     plugins: [
         babel({ babelHelpers: 'bundled' }),
     ],
